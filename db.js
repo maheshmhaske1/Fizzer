99a93c33d1c36cb69ec2f04ca484ab44d1221535
Mahesh mhaske <70438274+maheshmhaske1@users.noreply.github.com>
2022-08-11 12:26:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.mongo_string + process.env.dbchat_app, error => {
+mongoose.connect(process.env.mongo_string+process.env.dbchat_app, error => {
     if (error)
         throw error
     console.log(`server is listning on +=> http://localhost:${process.env.port}/`)
